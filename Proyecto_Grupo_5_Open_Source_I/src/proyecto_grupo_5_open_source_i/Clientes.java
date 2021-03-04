5de6ed599214aa2b5acc320b9f1800b115b2634f
Armando114 <71297507+Armando114@users.noreply.github.com>
2021-03-04 18:32:16 -0400

    Add files via upload

---
+++ Proyecto_Grupo_5_Open_Source_I/src/proyecto_grupo_5_open_source_i/Clientes.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_grupo_5_open_source_i;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author Armando
+ */
+@Entity
+@Table(name = "CLIENTES", catalog = "", schema = "ARMANDO")
+@NamedQueries({
+    @NamedQuery(name = "Clientes.findAll", query = "SELECT c FROM Clientes c")
+    , @NamedQuery(name = "Clientes.findByCodigo", query = "SELECT c FROM Clientes c WHERE c.codigo = :codigo")
+    , @NamedQuery(name = "Clientes.findByNombre", query = "SELECT c FROM Clientes c WHERE c.nombre = :nombre")})
+public class Clientes implements Serializable {
+
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "CODIGO")
+    private Integer codigo;
+    @Basic(optional = false)
+    @Column(name = "NOMBRE")
+    private String nombre;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idCliente2")
+    private Collection<Reservas> reservasCollection;
+
+    public Clientes() {
+    }
+
+    public Clientes(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Clientes(Integer codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        Integer oldCodigo = this.codigo;
+        this.codigo = codigo;
+        changeSupport.firePropertyChange("codigo", oldCodigo, codigo);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        String oldNombre = this.nombre;
+        this.nombre = nombre;
+        changeSupport.firePropertyChange("nombre", oldNombre, nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (codigo != null ? codigo.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Clientes)) {
+            return false;
+        }
+        Clientes other = (Clientes) object;
+        if ((this.codigo == null && other.codigo != null) || (this.codigo != null && !this.codigo.equals(other.codigo))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "proyecto_grupo_5_open_source_i.Clientes[ codigo=" + codigo + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+
+    public Collection<Reservas> getReservasCollection() {
+        return reservasCollection;
+    }
+
+    public void setReservasCollection(Collection<Reservas> reservasCollection) {
+        this.reservasCollection = reservasCollection;
+    }
+    
+}
